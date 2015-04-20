bfa5b4be037350a71c88912c4d65ad72e89df7c8
Vasanthy Kolluri <vasanthy@yahoo-inc.com>
2015-04-19 17:17:00 -0700

    Removing unused method

--- src/com/rock/yesman/networking/MyUtils.java
+++ src/com/rock/yesman/networking/MyUtils.java
@@ -5,10 +5,4 @@ public class MyUtils {
 		return ("ch" + userId.replace("@", "at"));
 	}
 
-	public static String getPhotoIdFromXml(String xml) {		
-		int startPosition = xml.indexOf("<photoid>") + "<photoid>".length();  
-		int endPosition = xml.indexOf("</photoid>", startPosition);  
-		String photoId = xml.substring(startPosition, endPosition);  
-		return photoId;
-	}
 }
