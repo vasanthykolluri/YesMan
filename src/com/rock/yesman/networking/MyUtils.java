788dc4446daf83c10a539408fc03a07c34a98a88
Vasanthy Kolluri <vasanthy@yahoo-inc.com>
2015-05-31 15:28:06 -0700

    Fixing channel name, manifest for ParsePush

--- src/com/rock/yesman/networking/MyUtils.java
+++ src/com/rock/yesman/networking/MyUtils.java
@@ -2,6 +2,7 @@ package com.rock.yesman.networking;
 
 public class MyUtils {
 	public static String getChannelName(String userId) {
-		return ("ch" + userId.replace("@", "at"));
+		//return ("ch" + userId.replace("@", "at"));
+		return userId;
 	}
 }
